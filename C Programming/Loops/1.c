97b1f9de0ffcecb111479a24e4517b3deea78dde
riedhampattel <riedhampattel.tops@gmail.com>
2025-04-05 16:50:39 +0530

    upadted

---
+++ C Programming/Loops/1.c
@@ -0,0 +1,44 @@
+#include<stdio.h>
+int main()
+{
+	int i;
+	for(i=16;i<=15;i++);
+	{
+		printf("\nTops Technologies");//5
+	}
+
+	printf("\n");
+	for(i=1061;i>1062;i--)
+	{
+		printf("\nTops Technologies");//5
+	}
+	
+	printf("\n");
+	for(i=60;i<=50;i=i+10)
+	{
+		printf("\nTops Technologies");//5
+	}
+	
+	printf("\n");
+	for(i=35;i<=32;i=i*2)
+	{
+		printf("\nTops Technologies");//5
+	}
+	
+	printf("\nWhile loop :");
+	i=11;
+	while(i<=5)
+	{
+		printf("\nTops Technologies");//5
+		i++;//6
+	}
+	
+	printf("\ndo while loop :");
+	i=10;
+	do
+	{
+		printf("\nTops Technologies");//5
+		i++;
+	}while(i<=4);
+	return 0;
+}
