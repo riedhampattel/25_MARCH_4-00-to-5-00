ecc9425c90a045ec011a7d62c435599ef1a94b86
riedhampattel <riedhampattel.tops@gmail.com>
2025-05-03 16:32:24 +0530

    updated

--- C++/Loops/6.cpp
+++ C++/Loops/6.cpp
@@ -1,7 +1,31 @@
 /*
 Write a program in C++ to find the sum of the series 1 +11 + 111 + 1111 + .. n terms.
 Sample Output:
-Input number of terms: 5
-1 + 11 + 111 + 1111 + 11111
+Input number of terms: 
+1 + 11 + 111 + 1111 + 11111 = 12345
 The sum of the series is: 12345
 */
+#include<iostream>
+using namespace std;
+int main()
+{
+    int term,i;
+    long int num = 1,sum=0;
+    cout<<"Enter the terms = ";
+    cin>>term;
+    for(i=1;i<=term;i++)
+    {
+        sum = sum + num;
+        cout<<num;
+        if(i<term)
+        {
+            cout<<" + ";
+        }
+        else
+        {
+            cout<<" = "<<sum;
+        }
+        num = (num*10) + 1;
+    }
+    return 0;
+}
--- C++/Loops/7.cpp
+++ C++/Loops/7.cpp
@@ -11,3 +11,48 @@ Write a c++ program to dislay this pattern
    * *
     *
 */
+#include<iostream>
+using namespace std;
+int main()
+{
+    int row,i,j,k;
+    up:
+    cout<<"\nEnter the row number = ";
+    cin>>row;//4
+    if(row%2==0)
+    {
+        cout<<"\nPlease enter odd numbers only!";
+        goto up;
+    }
+    int row1 = (row/2) + 1;
+    int row2 = row - row1;
+    int spc = row1-1;
+    int star = row2;
+    for(i=1;i<=row1;i++)
+    {
+        for(j=1;j<=spc;j++)
+        {
+            cout<<" ";
+        }
+        for(k=1;k<=i;k++)
+        {
+            cout<<"* ";
+        }
+        spc--;
+        cout<<endl;
+    }
+    for(i=1;i<=row2;i++)
+    {
+        for(j=1;j<=i;j++)
+        {
+            cout<<" ";
+        }
+        for(k=1;k<=star;k++)
+        {
+            cout<<"* ";
+        }
+        star--;
+        cout<<endl;
+    }
+    return 0;
+}
