3c80662e8905753679d726af7de61a56f835cb72
riedhampattel <riedhampattel.tops@gmail.com>
2025-05-27 16:45:56 +0530

    updated

--- DBMS/1.sql
+++ DBMS/1.sql
@@ -10,26 +10,33 @@ grade char not null
 
 describe student;
 
+-- rename table
 alter table student rename to employee;
 
 describe employee;
 
+-- rename table
 alter table employee rename to student;
 
+-- rename column
 alter table student rename column id to roll;
 
 describe student;
 
+-- change the data type of the column
 alter table student modify grade varchar(1);
 
 alter table student modify grade char not null;
 
 describe student;
 
+-- add column at last
 alter table student add column city varchar(20) not null;
 
+-- delete column
 alter table student drop column city;
 
+-- add column at specific position
 alter table student add column city varchar(20) not null after name;
 
 describe student;
