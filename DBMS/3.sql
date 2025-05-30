356c5383994344533852941a1d5b178646d98961
riedhampattel <riedhampattel.tops@gmail.com>
2025-05-30 16:48:51 +0530

    updated

--- DBMS/3.sql
+++ DBMS/3.sql
@@ -33,3 +33,49 @@ select * from employees where hire_date like '%-JU_-%';
 select first_name,last_name,salary*12 as annual_salary from employees;
 
 select first_name,last_name,salary,salary*comission_pct as comission_amnt,(salary*comission_pct) + salary as total_salary from employees;
+
+select first_name,last_name from employees;
+
+select concat(first_name,' ',last_name) as full_name from employees;
+
+select * from employees;
+
+select * from employees order by salary asc;
+
+select * from employees order by salary desc;
+
+select * from employees order by 2;
+
+select first_name,last_name,salary as income from employees order by income;
+
+select min(salary) from employees;
+
+select max(salary) from employees;
+
+select sum(salary) from employees;
+
+select avg(salary) from employees;
+
+select count(department_id) from employees;
+
+select count(comission_pct) from employees;
+
+select first_name,last_name,salary from employees where salary = (select min(salary) from employees);
+
+select first_name,last_name,salary from employees where salary = (select max(salary) from employees);
+
+select department_id,count(department_id) from employees group by DEPARTMENT_ID;
+
+select * from employees;
+
+select department_id,min(salary) from employees group by department_id;
+
+select department_id,min(salary) from employees group by DEPARTMENT_ID having min(salary)>5000;
+
+select * from employees order by salary desc limit 3;
+
+select * from employees limit 15 offset 5;
+
+select * from employees order by salary limit 10 offset 5;
+
+select * from employees where salary>10000 order by salary limit 5;
